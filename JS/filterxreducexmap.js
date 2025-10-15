b2cdbb4b5f3549f96d654a76334aac0b3eb99ef0
Gauri Sharma <gaurisharma89892@gmail.com>
2025-10-15 14:23:37 +0530

    adding js

---
+++ JS/filterxreducexmap.js
@@ -0,0 +1,7 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// Filter even numbers
+const evenNumbers = num.filter(n => n % 2 === 0);
+console.log("Even Numbers:", evenNumbers); // [2, 4, 6, 8, 10]
+  
